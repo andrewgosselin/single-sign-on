3130b522f6b7da45ebb061afc2726c18e71b0abf
Andrew Gosselin <andrew.gosselin@gray.tv>
2022-04-22 07:42:54 -0500

    changed multitenant to false

--- config/application/general.php
+++ config/application/general.php
@@ -1,5 +1,5 @@
 <?php
 return [
     "baseName" => env("APP_NAME", "Single Sign On"),
-    "multiTenant" => env('ENABLE_MULTI_TENANT', true)
+    "multiTenant" => env('ENABLE_MULTI_TENANT', false)
 ];

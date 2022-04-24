f3ad2f5644dac57d66c6b3d87469b60f382a3e77
Andrew Gosselin <andrew.gosselin@gray.tv>
2022-04-23 19:02:40 -0500

    Added landing

--- resources/views/layouts/unauthorized.blade.php
+++ resources/views/layouts/unauthorized.blade.php
@@ -16,6 +16,7 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     
+    <script src="https://kit.fontawesome.com/1f288f322f.js" crossorigin="anonymous"></script>
 
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
@@ -80,8 +81,9 @@
         </nav> -->
 
         <main>
-            @yield('content')
+            
         </main>
     </div>
+    @yield('content')
 </body>
 </html>
